0cfa9889d932325f90726ae70d37d9ad89f3cdd4
luanaparedes <lua123na@gmail.com>
2023-01-27 09:43:27 -0500

    added fields and columns to models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      //validate decimal
+    },
+    stock: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      // set default
+      //validate
+    },
+    category_id: {
+      type: DataType.INTEGER,
+      //reference 'category' for id
+    },
   },
   {
     sequelize,
